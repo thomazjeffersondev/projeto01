47bdf3c1e2c529ce70e5a4e51bfb9c0fee59b3b1
Thomaz Jefferson marins cabral <curso.python.dev@gmail.com>
2024-04-21 15:00:14 -0300

    Sistema de Castro nome e idade

---
+++ cadastro_de_nome.py
@@ -0,0 +1,8 @@
+def cadatro(texto1,texto2):
+
+    nome = input("Qual é o seu nome: ")
+    idade = int(input("Qual é a sua idade: "))
+
+    print(f"O meu nome é {nome} eu tenho {idade}")
+    return nome,idade
+cadatro(texto1="Thomaz Jefferson",texto2="35")
